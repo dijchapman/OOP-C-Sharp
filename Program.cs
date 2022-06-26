b6f5c1581530af00a755f485d141fec0d5ddc5cb
David <dijc2@kent.ac.uk>
2022-06-26 18:20:51 -0400

    Add performWorkout() method and associated methods to Workout class

--- Program.cs
+++ Program.cs
@@ -6,15 +6,16 @@ namespace workout_OOP {
         static void Main(string[] args) {
             Client peterParker = new Client("Peter", "Parker");
             Trainer tonyStark = new Trainer("Tony", "Stark");
-            peterParker.aboutMe();
-            tonyStark.aboutMe();
+            // peterParker.aboutMe();
+            // tonyStark.aboutMe();
 
             DateTime workoutDateTime = new DateTime(2022, 6, 30, 10, 30, 0);
             Workout workout = new Workout(peterParker, tonyStark, workoutDateTime, 60);
             workout.workoutDetails();
+            workout.performWorkout();
 
             Exercise benchPress = new Exercise("Bench Press", "Chest");
-            benchPress.exerciseDetails();
+            // benchPress.exerciseDetails();
         }
     }
 }
--- Workout.cs
+++ Workout.cs
@@ -16,4 +16,38 @@ public class Workout {
     public void workoutDetails() {
         Console.WriteLine("{0} will have a session with {1} on {2}, which will last {3} minutes", _client.FullName, _trainer.FullName, _dateAndTime, _duration);
     }
+
+    public void performWorkout() {
+        greetClient();
+        generalWarmup();
+        specificWarmup();
+        startExercises();
+        cooldownRoutine();
+        goodbyeToClient();
+    }
+
+    protected void greetClient() {
+        Console.WriteLine("Hello {0}, my name is {1} and I will be your personal trainer today.", _client.FullName, _trainer.FullName);
+    }
+
+    protected void generalWarmup() {
+        Console.WriteLine("To start, we will perform a 10 minute walk at a moderate speed.");
+    }
+
+    protected void specificWarmup() {
+        Console.WriteLine("Now we will perform specific stretches to prepare for the workout.");
+    }
+
+    protected void startExercises() {
+        Console.WriteLine("We will start performing the prescribed exercises for the session.");
+    }
+
+    protected void cooldownRoutine() {
+        Console.WriteLine("After the exercises, we will perform specific soft tissue foam rolling stretches.");
+        Console.WriteLine("To finish, we will perform a 5 minute walk at a moderate speed.");
+    }
+
+    protected void goodbyeToClient() {
+        Console.WriteLine("Goodbye {0}, I hope you enjoyed your workout and I hope to see you soon!", _client.FullName);
+    }
 }
